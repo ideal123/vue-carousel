6230a196e8a1964b3b9c172e59968dd01c401518
ideal123 <iseeucome@outlook.com>
2017-12-01 00:05:29 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>vue-carousel</h1>
+    <vue-carousel></vue-carousel>
+  </div>
+</template>
+
+<script>
+import VueCarousel from './vue-carousel'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: { VueCarousel }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  font-weight: normal;
+}
+</style>
